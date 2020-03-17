054a19be24205dd52dd6d1e7df801ae2adc80d8e
shanthosh <victorjohnkcah098@gmail.com>
2020-03-17 16:22:15 +0530

    try manifest

---
+++ src/main/java/com/mycompany/mavenproject1/mainClasser.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author Acer
+ */
+public class mainClasser {
+    public static void main(String a[]){
+        System.out.println("Haha");
+    }
+}
